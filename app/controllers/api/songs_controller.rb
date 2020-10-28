50472a3809b97d06fddd08459c210f54186557a2
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-28 17:12:45 -0500

    code update action and route

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -26,4 +26,22 @@ class Api::SongsController < ApplicationController
       render json: {errors: @song.errors.full_messages}
     end
   end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:title] || @song.title
+    @song.artist = params[:artist] || @song.artist
+    @song.genre = params[:genre] || @song.genre
+    @song.album = params[:album] || @song.album
+    @song.album_image = params[:album_image] || @song.album_image
+    @song.youtube_id = params[:youtube_id] || @song.youtube_id
+    @song.lyrics = params[:lyrics] || @song.lyrics
+    @song.tab = params[:tab] || @song.tab
+
+    if @song.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @song.errors.full_messages}
+    end
+  end
 end
