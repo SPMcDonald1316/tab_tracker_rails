60e0c176fb3c5ec6906fd0e03c5ad4cf1cfc479a
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-28 16:55:27 -0500

    code create route and action

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -8,4 +8,22 @@ class Api::SongsController < ApplicationController
     @song = Song.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @song = Song.new(
+      title: params[:title],
+      artist: params[:artist],
+      genre: params[:genre],
+      album: params[:album],
+      album_image: params[:album_image],
+      youtube_id: params[:youtube_id],
+      lyrics: params[:lyrics],
+      tab: params[:tab]
+    )
+    if @song.save
+      render 'show.json.jb'
+    else
+      render json: {errors: @song.errors.full_messages}
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,11 @@ Rails.application.routes.draw do
 
   namespace :api do
     post "/users" => "users#create"
+
     post "/sessions" => "sessions#create"
 
     get "/songs" => "songs#index"
     get "/songs/:id" => "songs#show"
+    post "/songs" => "songs#create"
   end
 end
